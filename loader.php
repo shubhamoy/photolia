1ae1914bb1370e40aef639b147ff9ed995b81591
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-04 08:58:51 +0530

    Add db check and install invoke

--- loader.php
+++ loader.php
@@ -1,5 +1,15 @@
 <?php
+    require_once('includes/helpers.php');
+
     spl_autoload_register(function ($class) {
         require_once('includes/'.ucfirst($class).'.php');
     });
     require_once('includes/Session.php');
+    
+    if(!check_db()) {
+        if($_SERVER['SCRIPT_NAME'] === '/install.php'){
+            
+        } else {
+            redirect_to('/install.php');
+        } 
+    } 
