ab5367e3f0122c41a95a5663bd0085964232699e
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-04 08:53:20 +0530

    SQL Queries for Table Creation

---
+++ includes/db.php
@@ -0,0 +1,40 @@
+<?php
+  $query = ["CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+);",
+"CREATE TABLE `pics` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `caption` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+);",
+"CREATE TABLE `comments` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `body` text,
+  `allowed` tinyint(1) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pid` (`pid`)
+);",
+"INSERT INTO `pics` VALUES (1,'docker_app.jpg','image/jpeg',323615,'Welcome to Photolia','welcome-to-photolia','2017-11-01 12:15:14','2017-11-01 12:15:14',NULL);",
+"INSERT INTO `comments` VALUES (1,1,'Master Yoda','Let the power be with you...',1,'2017-11-01 12:15:59','2017-11-01 12:15:59',NULL);"];
