a388309f7261ee7e8788e333224a19e7360aaf0c
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-04 08:42:51 +0530

    Use logout method

--- public/admin/logout.php
+++ public/admin/logout.php
@@ -1,8 +1,7 @@
 <?php
     require_once(__DIR__.'/../../includes/helpers.php');
     require_once(__DIR__.'/../../loader.php');
-    require_once(__DIR__.'/../../includes/Session.php');
     
-    Logger::start()->add(User::getUser($_SESSION['uid'])->username, $_SERVER['PHP_SELF'], 'Logout');
-    session_destroy();
-    redirect_to('login.php');
+    Logger::start()->add(User::getUser($_SESSION['uid'])->username, $_SERVER['PHP_SELF'], 'Logout', getIP());
+    $a = new Auth;
+    $a->logout();
